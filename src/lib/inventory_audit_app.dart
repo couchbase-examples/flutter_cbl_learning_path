b2ee676e864d3ccb6b7eed2b6ccacb4cb297f92e
Aaron LaBeau <alabeau@gmail.com>
2022-08-31 17:40:29 -0500

    updated to inject in repositories to main app

--- src/lib/inventory_audit_app.dart
+++ src/lib/inventory_audit_app.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_cbl_learning_path/features/audit/data/audit_repository.dart';
+import 'package:flutter_cbl_learning_path/features/audit/data/stock_item_repository.dart';
+import 'package:flutter_cbl_learning_path/features/project/data/project_repository.dart';
+import 'package:flutter_cbl_learning_path/features/project/data/warehouse_repository.dart';
 import 'package:flutter_cbl_learning_path/features/router/route.dart';
 import 'package:flutter_cbl_learning_path/features/database/database.dart';
 import './app_view.dart';
@@ -8,12 +12,20 @@ class InventoryAuditApp extends StatelessWidget {
   final FakeAuthenticationService authService;
   final AppRouterService routerService;
   final DatabaseProvider databaseProvider;
+  final ProjectRepository projectRepository;
+  final AuditRepository auditRepository;
+  final StockItemRepository stockItemRepository;
+  final WarehouseRepository warehouseRepository;
 
   const InventoryAuditApp(
       {Key? key,
       required this.authService,
       required this.routerService,
-      required this.databaseProvider})
+      required this.databaseProvider,
+      required this.projectRepository,
+      required this.auditRepository,
+      required this.stockItemRepository,
+      required this.warehouseRepository})
       : super(key: key);
 
   // This is the root of the application.
@@ -24,6 +36,10 @@ class InventoryAuditApp extends StatelessWidget {
           RepositoryProvider.value(value: authService),
           RepositoryProvider.value(value: routerService),
           RepositoryProvider.value(value: databaseProvider),
+          RepositoryProvider.value(value: projectRepository),
+          RepositoryProvider.value(value: auditRepository),
+          RepositoryProvider.value(value: stockItemRepository),
+          RepositoryProvider.value(value: warehouseRepository),
         ],
         child: MultiBlocProvider(
           providers: [
--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cbl_learning_path/features/audit/data/audit_repository.dart';
+import 'package:flutter_cbl_learning_path/features/audit/data/stock_item_repository.dart';
+import 'package:flutter_cbl_learning_path/features/project/data/project_repository.dart';
+import 'package:flutter_cbl_learning_path/features/project/data/warehouse_repository.dart';
 import './inventory_audit_app.dart';
 import 'features/router/route.dart';
 import 'features/database/database_provider.dart';
 
 void main() {
+  var dbProvider = DatabaseProvider();
+  var authService = FakeAuthenticationService();
+  //setup repositories
+  var stockItemRepository = StockItemRepository(dbProvider);
+  var warehouseRepository = WarehouseRepository(dbProvider);
+  var auditRepository = AuditRepository(dbProvider);
+  var projectRepository = ProjectRepository(dbProvider, authService,
+      auditRepository, warehouseRepository, stockItemRepository);
+
   runApp(InventoryAuditApp(
-      authService: FakeAuthenticationService(),
+      authService: authService,
       routerService: AppRouterService(),
-      databaseProvider: DatabaseProvider()));
+      databaseProvider: dbProvider,
+      projectRepository: projectRepository,
+      auditRepository: auditRepository,
+      stockItemRepository: stockItemRepository,
+      warehouseRepository: warehouseRepository));
 }
